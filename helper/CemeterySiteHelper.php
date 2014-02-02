10986c6445103637371f6e1379083087d3295eff
Robert Brockie <robertbrockie@gmail.com>
2014-02-02 09:52:54 -0500

    Write context horseshit

--- helper/CemeterySiteHelper.php
+++ helper/CemeterySiteHelper.php
@@ -4,7 +4,8 @@ class CemeterySiteHelper {
 
 	public static function getThumbnailURL($site) {
 
-		if (empty($site->getThumbnailImage())) {
+		$thumbnail_url = $site->getThumbnailImage();
+		if (empty($thumbnail_url)) {
 			return 'http://cemetery.townofbuchans.nf.ca/row_a/thumbnails/no_headstone.jpg';
 		} else {
 			return 'http://cemetery.townofbuchans.nf.ca/row_'.strtolower($site->getRow()).'/'.$site->getThumbnailImage();
@@ -12,7 +13,9 @@ class CemeterySiteHelper {
 	}
 
 	public static function getFullImageURL($site) {
-		if (empty($site->getFullImage())) {
+
+		$full_image_url = $site->getFullImage();
+		if (empty($full_image_url)) {
 			return 'http://cemetery.townofbuchans.nf.ca/row_a/thumbnails/no_headstone.jpg';
 		} else {
 			return 'http://cemetery.townofbuchans.nf.ca/row_'.strtolower($site->getRow()).'/'.$site->getFullImage();
